17e042a1158763bd8de575433a942886897883ec
rubnsMac <rubns.s24@gmail.com>
2020-02-07 17:04:45 -0600

    prueba

--- practica10/index.php
+++ practica10/index.php
@@ -7,9 +7,26 @@
     <title>Document</title>
 </head>
 <body>
+    <div>
+        <div id="div1"></div>
+        <button id="add">Sumar</button>
+        <button id="res">Restar</button>
+        <button id="mul">Multiplicar</button>
+        <button id="div">Dividir</button>
+    </div>
+    <label for=""> Ingrese A: <input type="text" id="a"></label>
+    <label for=""> Ingrese B: <input type="text" id="b"></label>
+    <label for=""> Resultado: <input type="text" id="c"></label>
     <script src="js/jquery-3.4.1.min.js"></script>
-    <script >
-        $(document).ready()
-    </script>
+    <script src="js/operaciones.js"></script>
+   <!--  <script >
+        $(document).ready( function (){
+            x();
+            $("#div1").html("<p>hola mundo <p>")
+        })
+        function x(){
+            alert("hola")
+        }
+    </script> -->
 </body>
 </html>
---
+++ practica10/js/operaciones.js
@@ -0,0 +1,25 @@
+$("#add").click(() =>{ 
+    let a = parseFloat($("#a").val())
+    let b = parseFloat($("#b").val())
+    let c = a+b
+    $("#c").val(c)
+});
+$("#div").click(() =>{ 
+    let a = parseFloat($("#a").val())
+    let b = parseFloat($("#b").val())
+    let c = a/b
+    $("#c").val(c)
+});
+$("#mul").click(() =>{ 
+    let a = parseFloat($("#a").val())
+    let b = parseFloat($("#b").val())
+    let c = a*b
+    $("#c").val(c)
+});
+$("#res").click(() =>{ 
+    let a = parseFloat($("#a").val())
+    let b = parseFloat($("#b").val())
+    let c = a-b
+    $("#c").val(c)
+});
+ 
